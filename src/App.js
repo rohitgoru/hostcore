7670fbfc1bdbec924a642b808fadf11f64ae0212
gagan <gagandharmk@gmail.com>
2024-03-09 18:56:27 +0530

    Home Component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Home from "./commponents/home/Home";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h2 className="text-center text-2xl text-green-700">hello react</h2>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
---
+++ src/commponents/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <div className="text-red-400">this is the home page</div>
+    </div>
+  );
+};
+
+export default Home;
