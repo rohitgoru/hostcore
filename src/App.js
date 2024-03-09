386cd9b420a4841237c23c22434f5c602c462cb9
rohit <rohit780790809@gmail.com>
2024-03-09 10:17:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h2 className="text-center text-2xl text-green-700">hello react</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
